db170c3b9a34bfd143a0ef92becce3324f7f3f1c
Mike Hotchkiss <michael.hotchkiss@protonmail.com>
2021-01-28 12:55:06 -0500

    Setup state and created the starting state

---
+++ Assets/State.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This allows for use in unity right click to create
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    //Text area sets size of text field in Unity. First = min Second = amount of lines before scroll
+    [TextArea(14,10)] [SerializeField] string StoryText;
+}
